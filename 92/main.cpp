8b77ba57b1dbc88a563d0e63ef9f66f0ac0ec885
Zeyi Zhao <zeyi.zhao@gmail.com>
2015-07-24 22:04:23 +0800

    pass #92

--- 92/main.cpp
+++ 92/main.cpp
@@ -24,8 +24,26 @@ ListNode* reverseBetween(ListNode* head, int m, int n) {
         lastnode = it;
         it = tmp;
     }
+    if(before)
+        before->next = lastnode;
+    start->next = it;
+    if(before)
+        return head;
+    else
+        return lastnode;
 }
 
  int main () {
+    ListNode* l1 = new ListNode(1), *l2 = new ListNode(2), * l3 = new ListNode(3), *l4 = new ListNode(4),*l5 = new ListNode(5);
+    l1->next = l2;
+    l2->next = l3;
+    l3->next = l4;
+    l4->next = l5;
+    ListNode * l = reverseBetween(l1, 2, 5);
+    while(l) {
+        cout<<l->val<<'\t';
+        l = l->next;
+    }
+    cout<<endl;
     return 0;
  }
