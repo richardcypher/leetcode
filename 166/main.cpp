871968464af8b5fe3b432744e67be2b9c10068ea
Zeyi Zhao <zeyi.zhao@gmail.com>
2015-10-19 20:56:35 +0800

    add #166

---
+++ 166/main.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年10月18日 星期日 14时10分28秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string fractionToDecimal(int numerator, int denominator) {
+    
+}
+
+int main() {
+    return 0;
+}
