cf807d44c88363dc5aba5c0adbd9179c9e575f13
Zeyi Zhao <zeyi.zhao@gmail.com>
2015-10-20 21:02:08 +0800

    pass #166

--- 166/main.cpp
+++ 166/main.cpp
@@ -11,7 +11,7 @@
 #include<map>
 using namespace std;
 
-string num2str(long num) {
+string num2str(long long num) {
     stringstream ss;
     ss<<num;
     return ss.str();
@@ -19,74 +19,46 @@ string num2str(long num) {
 
 string fractionToDecimal(int numerator, int denominator) {
     string str = "";
-    if(numerator == -2147483648 && denominator == -1)
-        return "2147483648";
-    if(numerator % denominator == 0)
+    if(denominator == 0)
+        return str;
+    bool pos = false;
+    if((numerator >= 0 && denominator > 0) || (numerator <= 0 && denominator < 0))
+        pos = true;
+    long long numerator1 = numerator, denominator1 = denominator;
+    if(numerator1 < 0)
+        numerator1 *= -1;
+    if(denominator1 < 0)
+        denominator1 *= -1;
+    if(numerator1 % denominator1 == 0)
     {
-       return num2str(numerator / denominator); 
+        long long num = numerator1 / denominator1;
+        str = num2str(num); 
+        if(!pos)
+            str = "-" + str;
+       return str;
     }
-    else if(numerator > denominator) {
-        map<int, int> lefts;
-        str = num2str(numerator / denominator);
-        str += ".";
-        int left = numerator % denominator;
-        int divided;
-        while(left != 0) {
-            left *= 10;
-            if(lefts.find(left) != lefts.end()) {
-                str = str.substr(0, lefts[left]) + "(" + str.substr(lefts[left]) + ")";
-                return str;
-            }
-            divided = left / denominator;
-            str += num2str(divided);
-            lefts[left] = str.length() - 1;
-            left = left % denominator;
+    map<long, int> lefts;
+    str = num2str(numerator1 / denominator1);
+    str += ".";
+    long  long left = numerator1 % denominator1;
+    int divided;
+    while(left != 0) {
+        left *= 10;
+        if(lefts.find(left) != lefts.end()) {
+            str = str.substr(0, lefts[left]) + "(" + str.substr(lefts[left]) + ")";
+            break;
         }
+        divided = left / denominator1;
+        str += num2str(divided);
+        lefts[left] = str.length() - 1;
+        left = left % denominator1;
     }
-    else if(numerator < denominator) {
-        str = "0.";
-        int left = numerator * 10;
-        string ss = "";
-        while(left < denominator) {
-            left *= 10;
-            ss += "0";
-        }
-        string lnum, rnum;
-        string dividedstr = fractionToDecimal(left, denominator);
-        dividedstr = ss + dividedstr;
-        for(int i = 0;i < dividedstr.length(); i++) {
-            if(dividedstr[i] == '.') {
-                lnum = lnum + dividedstr.substr(0, i);
-                dividedstr.erase(i, 1);
-                break;
-            }
-        }
-        size_t l = dividedstr.find_first_of("(");
-        if(l != string::npos) {
-            rnum = dividedstr.substr(l + 1);
-            rnum.erase(rnum.length() - 1);
-            if(l == lnum.length()) {
-                int lastl = l - 1, lastr = rnum.length() - 1;
-                int move = 0;
-                while(lastl >= 0 && lastr >= 0 && lnum[lastl] == rnum[lastr])
-                {
-                    lastl--;
-                    lastr--;
-                    move++;
-                }
-                for(int i = 0; i < move; i++)
-                    dividedstr[l - i] = dividedstr[l - i - 1];
-                dividedstr[l - move] = '(';
-                dividedstr[dividedstr.length() - 1 - move] = ')';
-                dividedstr.erase(dividedstr.length() - move);
-            }
-        }
-        str += dividedstr;
-    }
+    if (!pos)
+        str = "-" + str;
     return str;
 }
 
 int main() {
-    cout<<fractionToDecimal(1, 99)<<endl;
+    cout<<fractionToDecimal(7, -12)<<endl;
     return 0;
 }
