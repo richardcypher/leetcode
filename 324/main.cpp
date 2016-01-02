508738b4f4f806750aac7b130e528989b20cdf48
Zeyi Zhao <zeyi.zhao@gmail.com>
2016-01-02 15:01:41 +0800

    pass #324 Wiggle Sort II

---
+++ 324/main.cpp
@@ -0,0 +1,36 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年01月02日 星期六 14时09分42秒
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include <algorithm> 
+using namespace std;
+
+void wiggleSort(vector<int>& nums) {
+    int n = nums.size();
+
+    // Find a median.
+    auto midptr = nums.begin() + n / 2;
+    nth_element(nums.begin(), midptr, nums.end());
+    int mid = *midptr;
+
+    // Index-rewiring.
+    #define A(i) nums[(1+2*(i)) % (n|1)]
+
+    // 3-way-partition-to-wiggly in O(n) time with O(1) space.
+    int i = 0, j = 0, k = n - 1;
+    while (j <= k) {
+        if (A(j) > mid)
+            swap(A(i++), A(j++));
+        else if (A(j) < mid)
+            swap(A(j), A(k--));
+        else
+            j++;
+    }
+}
+
+int main(){}
